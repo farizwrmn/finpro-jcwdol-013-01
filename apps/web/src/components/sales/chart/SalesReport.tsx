b472ca17636afa6526947f840816244a4f469e2a
AlexManurung59 <alexvirdo59@gmail.com>
2024-06-15 11:28:40 +0700

    feat: sales report

--- apps/web/src/components/sales/chart/SalesReport.tsx
+++ apps/web/src/components/sales/chart/SalesReport.tsx
@@ -4,7 +4,14 @@ import React from 'react';
 import BarChart from './BarChart';
 import LineChart from './LineChart';
 import PieChart from './PieChart';
-import { AspectRatio, Box, Grid, GridItem } from '@chakra-ui/react';
+import {
+  AspectRatio,
+  Box,
+  Center,
+  Grid,
+  GridItem,
+  Text,
+} from '@chakra-ui/react';
 
 // Sales Data Interface (replace with your actual data structure)
 interface SalesData {
@@ -32,6 +39,19 @@ const DashboardSalesReport: React.FC = () => {
     'May',
     'June',
     'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const monthDatasets = [
+    {
+      label: 'Penjualan',
+      data: monthLabels.map(() => 100),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
   ];
 
   const categoryDatasets = [
@@ -62,11 +82,51 @@ const DashboardSalesReport: React.FC = () => {
     },
   ];
 
-  const categoryLabels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
+  const categoryLabels = [
+    'Sayur',
+    'Buah',
+    'Daging',
+    'Minyak',
+    'Garam',
+    'Telur',
+  ];
 
-  const pieDatasets = [
+  const productLabels = [
+    'Cap Kaki Naga',
+    'Cap Buah Khuldi',
+    'Daging Semut Abadi',
+    'Minyak Cap Keikhlasan',
+    'Garam Anti Miskin',
+    'Telur Dinosaurus Gede Sebelah',
+  ];
+
+  const categoryPieDatasets = [
     {
-      label: '# of Votes',
+      label: 'Penjualan',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ];
+
+  const productPieDatasets = [
+    {
+      label: 'Penjualan',
       data: [12, 19, 3, 5, 2, 3],
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
@@ -90,25 +150,32 @@ const DashboardSalesReport: React.FC = () => {
 
   return (
     <Box className="dashboard-sales-report">
-      <h1>Sales Report</h1>
-      <div className="sales-summary"></div>
-
       {/* Charts Section */}
-      <h2>Sales by Month</h2>
       <Grid templateColumns="repeat(auto-fit,minmax(300px,1fr">
         <Grid templateColumns="repeat(auto-fit, minmax(300px, 1fr))" gap={4}>
           <GridItem colSpan={1}>
-            <Box w="full" mb={8}>
+            <Box w="full" mb={-150}>
               <AspectRatio>
                 <BarChart
-                  title="Laporan Penjualan Per Kategori"
+                  title="Laporan Penjualan Per Bulan"
                   labels={monthLabels}
-                  datasets={categoryDatasets}
+                  datasets={monthDatasets}
                 />
               </AspectRatio>
             </Box>
           </GridItem>
         </Grid>
+        <GridItem colSpan={1}>
+          <Box w="full" mb={-150}>
+            <AspectRatio>
+              <BarChart
+                title="Laporan Penjualan Per Kategori"
+                labels={monthLabels}
+                datasets={categoryDatasets}
+              />
+            </AspectRatio>
+          </Box>
+        </GridItem>
         <GridItem colSpan={1}>
           <Box w="full" mb={8}>
             <AspectRatio ratio={16 / 9}>
@@ -123,14 +190,33 @@ const DashboardSalesReport: React.FC = () => {
       </Grid>
 
       {/* Additional Charts (optional) */}
-      <GridItem colSpan={1}>
-        <h2>Top Selling Products (Pie Chart)</h2>
-        <Box w="full" mb={8}>
-          <AspectRatio ratio={16 / 9}>
-            <PieChart labels={categoryLabels} datasets={pieDatasets} />
-          </AspectRatio>
-        </Box>
-      </GridItem>
+
+      <Grid templateColumns="repeat(auto-fit, minmax(300px, 1fr))" gap={4}>
+        <GridItem colSpan={1}>
+          <Box w="full" mb={8}>
+            <Text align="center" mb={5}>
+              Penjualan Per Kategori
+            </Text>{' '}
+            <AspectRatio ratio={1}>
+              <PieChart
+                labels={categoryLabels}
+                datasets={categoryPieDatasets}
+              />
+            </AspectRatio>
+          </Box>
+        </GridItem>
+
+        <GridItem colSpan={1}>
+          <Box w="full" mb={8}>
+            <Text align="center" mb={5}>
+              Penjualan Per Produk
+            </Text>
+            <AspectRatio ratio={1}>
+              <PieChart labels={productLabels} datasets={productPieDatasets} />
+            </AspectRatio>
+          </Box>
+        </GridItem>
+      </Grid>
     </Box>
   );
 };
