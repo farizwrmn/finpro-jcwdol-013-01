8403180356c84d06e86301b5bea59cd28525108d
Alex Manurung <157109199+AlexManurung59@users.noreply.github.com>
2024-07-05 19:29:48 +0700

    Fpj00 5 (#93)
    * feat: change password page
    
    * build: create reset password page
    
    * Update page.tsx
    
    * feat: user profile
    
    * feat: add user profile api
    
    * chore: git pull develop
    
    * feat: edit profile and input product details page
    
    * feat: add upload avatar
    
    * refactor: split local storage token & user
    
    * feat: add products page
    
    * feat: create sales dashboard and bar chart dummy
    
    * feat: sales change code
    
    * feat: sales report
    
    * feat: create Cart
    
    * chore: add swiper package
    
    * feat: create checkout,shipping,payment page
    
    * feat: create order and fix checkout
    
    * feat: order page
    
    * feat: fix order page
    
    * feat: edit html email and reset password
    
    * feat: create success payment and failed payment
    
    * feat: create mapbox
    
    * feat: create logo and change favicon
    
    * feat: fix icon and logo
    
    * feat: create sales report be
    
    * feat: sales report per product
    
    * feat: welcome word at admin dashboard
    
    ---------
    
    Co-authored-by: Rehan <rehanadipurwana@gmail.com>

--- apps/web/src/app/admin/(dashboard)/page.tsx
+++ apps/web/src/app/admin/(dashboard)/page.tsx
@@ -1,22 +1,14 @@
-import { Text, Image } from '@chakra-ui/react';
+import { Text, Image, Box, Flex } from '@chakra-ui/react';
 import React from 'react';
 
 const Page = () => {
   return (
-    <div>
-      <Text fontSize="3xl" fontFamily="monospace" fontWeight="bold">
-        Admin Dashboard
+    <Flex justifyContent="center" alignItems="center" flexDirection="column">
+      <Text fontSize="5xl" fontFamily="monospace" fontWeight="bold">
+        Welcome to Admin Dashboard
       </Text>
-      <Image
-        rounded={'md'}
-        alt={'feature image'}
-        src={
-          'https://media.istockphoto.com/id/509981891/vector/monkey-with-a-banana.jpg?s=612x612&w=0&k=20&c=WE-R9YhMggcE0FfppCVmfrqje_APsJR5J_TIbEYSd3M='
-        }
-        width={250}
-        mt={10}
-      />
-    </div>
+      <Image src="/assets/images/admindashboard.jpg" alt="welcome" w={500} />
+    </Flex>
   );
 };
 
--- apps/web/src/app/users/page.tsx
+++ apps/web/src/app/users/page.tsx
@@ -1,31 +1,16 @@
-import { Button, Stack, Text } from '@chakra-ui/react';
+import { Button, Flex, Stack, Text, Image } from '@chakra-ui/react';
 import Link from 'next/link';
 import React from 'react';
 
 const Page = () => {
   return (
     <div>
-      <Text fontSize="3xl" fontFamily="monospace" fontWeight="bold">
-        User Dashboard
-      </Text>
-      <Stack flex={'1'} flexDirection={'row'} mt={5} align={'center'}>
-        <Link href="/">
-          <Button
-            variant={'none'}
-            color={'teal'}
-            size={'md'}
-            _hover={{ textDecoration: 'underline' }}
-            mr={-4}
-          >
-            Homepage
-          </Button>
-        </Link>
-        <Text>/</Text>
-        <Text as={'b'} decoration={'underline'}>
-          {' '}
-          Dashboard
+      <Flex justifyContent="center" alignItems="center" flexDirection="column">
+        <Text fontSize="5xl" fontFamily="monospace" fontWeight="bold">
+          Welcome to User Dashboard
         </Text>
-      </Stack>
+        <Image src="/assets/images/userdashboard.svg" alt="welcome" w={500} />
+      </Flex>
     </div>
   );
 };
